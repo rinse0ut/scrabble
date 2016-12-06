62af930567f3c69c64a442a2851e40cc2627d105
Dave Thomson <djthomson@gmail.com>
2016-12-06 08:56:11 +0000

    Fix page test

--- components/Page.js
+++ components/Page.js
@@ -2,21 +2,22 @@ import React, { Component, PropTypes } from 'react'
 import R from 'ramda'
 
 const P = (text, key) => <p key={key}>{text}</p>
+const mapIndexed = R.addIndex(R.map)
 
 const Page = ({ title, paragraphs }) => {
     return (
         <div>
             <h1>{title}</h1>
             {
-                R.map(P, paragraphs)
+                mapIndexed(P, paragraphs)
             }
         </div>
     )
 }
 
-// Page.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   paragraphs: PropTypes.array.isRequired
-// }
+Page.propTypes = {
+  title: PropTypes.string.isRequired,
+  paragraphs: PropTypes.array.isRequired
+}
 
 export default Page
